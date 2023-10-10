f8ba7ef9629414423d011fc2f305b7c05d44e3eb
Your Name <09taumang@gmail.com>
2023-10-10 16:10:16 +0200

    All endpoints are functional

--- Models/HoneyInventory.cs
+++ Models/HoneyInventory.cs
@@ -1,6 +1,7 @@
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Text;
 using System.Threading.Tasks;
 
 namespace honey_inventory_mongdb.Models
@@ -20,5 +21,25 @@ namespace honey_inventory_mongdb.Models
 
     public string Honey_Size { get; set; } = null!;
     public int Number_of_Bottles { get; set; }
+    // Auto-generates the Id characters for the ID 
+    //  public HoneyInventory()
+    // {
+    //     Id = GenerateId();
+    // }
+
+    // private string GenerateId()
+    // {
+    //     const string chars = "abcdefghijklmnopqrstuvwxyz0123456789";
+    //     var random = new Random();
+    //     var builder = new StringBuilder(24);
+
+    //     for (int i = 0; i < 24; i++)
+    //     {
+    //         builder.Append(chars[random.Next(chars.Length)]);
+    //     }
+
+    //     return builder.ToString();
+    // }
+
     }
 }
